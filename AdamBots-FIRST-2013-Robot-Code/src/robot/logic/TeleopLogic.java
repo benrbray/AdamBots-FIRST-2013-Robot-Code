7a71500ae8ed30a39f0ee75af7891f6cc658a439
Steven <Steven@10.0.1.7>
2013-01-23 17:15:12 -0500

    Method skeletons for TeleopLogic.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/logic/TeleopLogic.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/TeleopLogic.java
@@ -4,10 +4,61 @@
 
 package robot.logic;
 
+import robot.control.FancyJoystick;
+import robot.control.MagicBox;
+
 /**
  *
  * @author Steven
  */
 public class TeleopLogic {
     
+    //// CONSTANTS -------------------------------------------------------------
+    
+    //// VARIABLES -------------------------------------------------------------
+    private FancyJoystick _primaryJoy;
+    private FancyJoystick _secondaryJoy;
+    private MagicBox _magicBox;
+    
+    /**
+     * Creates an instance of TeleopLogic.
+     */
+    public TeleopLogic() {
+	
+    }
+    
+    /**
+     * Initializes variables, objects, etc to their starting states.
+     */
+    public void teleopInit() {
+	
+    }
+    
+    /**
+     * Update method. To be called periodically by MainControl.
+     */
+    public void tick() {
+	
+    }
+    
+    /**
+     * Gathers primary joystick values.
+     */
+    private void updatePrimaryJoystick() {
+	
+    }
+    
+    /**
+     * Gathers secondary joystick values.
+     */
+    private void updateSecondaryJoystick() {
+	
+    }
+    
+    /**
+     * Gathers magic box values.
+     */
+    private void updateMagicBox() {
+	
+    }
 }

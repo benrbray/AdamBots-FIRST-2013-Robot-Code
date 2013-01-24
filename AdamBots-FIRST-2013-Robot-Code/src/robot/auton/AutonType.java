e3320f9482c7188d698159bd3de9232ec4798d0e
Ben Bray <archon68@gmail.com>
2013-01-23 22:21:21 -0500

    Began working on AutonLogic and associated classes.

---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/auton/AutonType.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.auton;
+
+/**
+ *
+ * @author Ben
+ */
+public abstract class AutonType {
+    
+    protected AutonTask[] _tasks;
+    
+    //// CONSTRUCTOR -----------------------------------------------------------
+    
+    public AutonType(){
+	
+    }
+    
+    //// METHODS ---------------------------------------------------------------
+    
+    public void next(int status){
+	
+    }
+}

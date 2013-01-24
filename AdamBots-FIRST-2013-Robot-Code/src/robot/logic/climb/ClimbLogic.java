f2571ca94912ba82654bb7734cb6d5d4e442595f
Ben Bray <archon68@gmail.com>
2013-01-23 22:34:35 -0500

    Initial structural changes based on 1/23/2013 code structure diagram.

---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/climb/ClimbLogic.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.logic.climb;
+
+/**
+ *
+ * @author Ben
+ */
+public class ClimbLogic {
+    
+}
--- AdamBots-FIRST-2013-Robot-Code/src/robot/logic/teleop/TeleopLogic.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/teleop/TeleopLogic.java
@@ -2,7 +2,7 @@
  * This class handles the updateJoystick Methods as well as the update magic box methods from last year.
  */
 
-package robot.logic;
+package robot.logic.teleop;
 
 /**
  *

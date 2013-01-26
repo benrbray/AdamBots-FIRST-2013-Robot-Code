13f843ccf1688d76a6eec2b68c584c616d5b0f06
Ben Bray <archon68@gmail.com>
2013-01-26 13:14:07 -0500

    added publish script to gh-pages, hopefully it'll resume execution in place

---
+++ publish-javadoc.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Push javadoc files to a website hosted by GitHub.
+# Before executing this script, generate the javadoc files into 
+# AdamBots-FIRST-2013-Robot-Code/javadoc
+
+# verbose output
+set -v
+
+# commit changes
+git commit -a
+git push
+
+# find out the current branch so we know where to switch back
+OLD_BRANCH=`git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+
+echo $OLD_BRANCH
+
+git checkout gh-pages || exit $?
+
+# Clear out the old files: (files which will be served)
+rm -rf ./javadoc/* 
+
+# Copy files to this branch
+git checkout $OLD_BRANCH ./javadoc/*
+
+# Add and Commit Javadoc
+git add -A
+git commit -a -m "generated javadoc"
+
+git push origin gh-pages || exit $?
+
+# Switch back to the old branch
+git checkout $OLD_BRANCH || exit $?

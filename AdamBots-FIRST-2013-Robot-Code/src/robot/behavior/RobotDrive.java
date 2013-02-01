09e1ac00c36beb13d2bb9c3bbba26eb7e4fb72f4
Tyler <Tyler@10.251.138.72>
2013-02-01 16:20:07 -0500

    Added code to all the methods.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotDrive.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotDrive.java
@@ -16,16 +16,23 @@ public class RobotDrive {
     // sensors, motors
     // take out the next few lines when static class is made
     public final int PORT_LEFT_VICTOR = 1;
-    public final int PORT_RIGHT_VICTOR = 1;
+    public final int PORT_RIGHT_VICTOR = 2;
     
-    private Victor _leftVictor = new Victor( PORT_LEFT_VICTOR );
-    private Victor _rightVictor = new Victor( PORT_RIGHT_VICTOR );
+    private Victor _leftVictor;
+    private Victor _rightVictor;
     
     
     public RobotDrive( )
     {
-	
-	
+	robotDriveInit();
+    }
+    
+    /**
+     * Initialize everything
+     */
+    private void robotDriveInit() {
+	_rightVictor = new Victor( PORT_RIGHT_VICTOR );
+	_leftVictor = new Victor( PORT_LEFT_VICTOR );
     }
     
     /**
@@ -34,9 +41,9 @@ public class RobotDrive {
      * @param rightSpeed to set the right speed
      * sets the speed of the wheels to the parameters given
      */
-    public void drive( double leftSpeed, double rightSpeed )
-    {
-	
+    public void drive( double leftSpeed, double rightSpeed ) {
+	_rightVictor.set(rightSpeed);
+	_leftVictor.set(leftSpeed);
     }
     
     /**
@@ -46,7 +53,8 @@ public class RobotDrive {
      */
     public void driveStraight( double speed )
     {
-
+	_rightVictor.set(speed);
+	_leftVictor.set(speed);
     }
     
     /**
@@ -56,15 +64,18 @@ public class RobotDrive {
      */
     public void turn( double speed )
     {
-	
+	_rightVictor.set(speed*-1);
+	_leftVictor.set(speed);
     }
     
     /**
+     * 
      * stops all the motors
      */
     public void stop()
     {
-	
+	_rightVictor.set(0);
+	_leftVictor.set(0);
     }
     
 }

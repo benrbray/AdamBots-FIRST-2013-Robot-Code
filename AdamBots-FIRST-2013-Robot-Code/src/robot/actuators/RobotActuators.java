635f55deb0e9fe9c9d97142ddf996098585324aa
Ben Bray <archon68@gmail.com>
2013-02-01 16:59:33 -0500

    Commented RobotActuators.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/actuators/RobotActuators.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/actuators/RobotActuators.java
@@ -25,27 +25,39 @@ public class RobotActuators {
     
     //// DRIVE -----------------------------------------------------------------
     
+    /** Left drive Victor. */
     public Victor driveLeft;
+    /** Right drive Victor. */
     public Victor driveRight;
     
+    /** Left transmission Servo.*/
     public Servo transmissionLeft;
+    /** Right transmission Servo. */
     public Servo transmissionRight;
     
     //// CLIMBING --------------------------------------------------------------
     
+    /** Climb pivot motor. */
     public Victor climbPivot;
+    /** Climb left winch motor. */
     public Victor climbWinchLeft;
+    /** Climb right winch motor. */
     public Victor climbWinchRight;
     
     //// DISC ACQUISITION ------------------------------------------------------
     
+    /** Disc elevator motor. */
     public Victor discElevator;
+    /** Spike for disc lift system. */
     public Relay discLift;
     
     //// SHOOTER ---------------------------------------------------------------
     
+    /** Left shooter wheel motor. */
     public Victor shooterWheelLeft;
+    /** Right shooter wheel motor. */
     public Victor shooterWheelRight;
+    /** Controls the angle of attack of the shooter. */
     public Victor shooterAngleMotor;
     
     

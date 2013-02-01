6fae0b429f2bbb975ea562dabd8033bce3a00953
Ben Bray <archon68@gmail.com>
2013-02-01 15:37:35 -0500

    Added RobotActuators class.  This class will contain static instances of actuators.

---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/acuators/RobotActuators.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.acuators;
+
+/**
+ * Contains static instances of robot actuators.
+ * @author Ben
+ */
+public class RobotActuators {
+    
+    //// INITIALIZATION --------------------------------------------------------
+    
+    /**
+     * Initializes static actuator instances.
+     */
+    public void init(){
+	
+    }
+    
+    //// DRIVE -----------------------------------------------------------------
+    
+}

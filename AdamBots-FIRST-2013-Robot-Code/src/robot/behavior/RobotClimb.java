27e3cac95fe89f0fed07600fb80c9f6a4efb2e45
Curtis Fenner <blueshamenblutek@gmail.com>
2013-02-04 16:56:44 -0500

    Created the RobotClimb class which gives functionality to the solenoid and winch.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotClimb.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotClimb.java
@@ -4,10 +4,44 @@
  */
 package robot.behavior;
 
+import edu.wpi.first.wpilibj.Relay;
+import robot.actuators.RobotActuators;
+import robot.sensors.RobotSensors;
+
 /**
  *
- * @author Ben
+ * @author Curtis
  */
 public abstract class RobotClimb {
     
+    private static double _winchSpeed = 0;
+    /**
+     * Set the target speed of the winch (e.g., 0.5 for reel out, -0.5 for reel in)
+     * @param speed The PWM speed to set it at.
+     */
+    public static void setWinchSpeed(double speed)
+    {
+        _winchSpeed = speed;
+    }
+    public static void update()
+    {
+        if (RobotSensors.limitWinch1.get()) //TODO: Determine 1 vs 2 for limits, winch speed pairing.
+        {
+            _winchSpeed = Math.max(0,_winchSpeed);
+        }
+        if (RobotSensors.limitWinch2.get()) //TODO: Determine 1 vs 2 for limits, winch speed pairing.
+        {
+            _winchSpeed = Math.min(0,_winchSpeed);
+        }
+        RobotActuators.climbWinch.set(_winchSpeed);
+    }
+    
+    public static void startSolenoid()
+    {
+        RobotActuators.climbWinchRelease.set(Relay.Value.kOn);
+    }
+    public static void stopSolenoid()
+    {
+        RobotActuators.climbWinchRelease.set(Relay.Value.kOn);
+    }
 }

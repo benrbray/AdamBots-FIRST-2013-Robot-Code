d978150ff554fc6ae03b397e0ecc930e7a648c7b
Curtis Fenner <blueshamenblutek@gmail.com>
2013-02-04 17:26:15 -0500

    Replaced setSpeed with setTarget[Position] in the RobotClimb class for specifying control of the winch.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotClimb.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotClimb.java
@@ -14,26 +14,43 @@ import robot.sensors.RobotSensors;
  */
 public abstract class RobotClimb {
     
-    private static double _winchSpeed = 0;
     /**
-     * Set the target speed of the winch (e.g., 0.5 for reel out, -0.5 for reel in)
+     * The tolerance of the winch targeting.
+     */
+    public static final double TOLERANCE = 0.5;
+    
+    private static double _winchTarget = 0;
+    /**
+     * Set the target position (inches?) of the winch (e.g., 0 for reel in, 15 for reel out)
      * @param speed The PWM speed to set it at.
      */
-    public static void setWinchSpeed(double speed)
+    public static void setWinchTarget(double target)
     {
-        _winchSpeed = speed;
+        _winchTarget = target;
+    }
+    private static double getWinchPosition()
+    {
+        return RobotSensors.encoderWinch.getDistance();
     }
     public static void update()
     {
+        
         if (RobotSensors.limitWinch1.get()) //TODO: Determine 1 vs 2 for limits, winch speed pairing.
         {
-            _winchSpeed = Math.max(0,_winchSpeed);
+            _winchTarget = Math.min(_winchTarget,getWinchPosition()-TOLERANCE/2);
         }
-        if (RobotSensors.limitWinch2.get()) //TODO: Determine 1 vs 2 for limits, winch speed pairing.
+        if (RobotSensors.limitWinch2.get())
         {
-            _winchSpeed = Math.min(0,_winchSpeed);
+            _winchTarget = Math.max(_winchTarget,getWinchPosition()+TOLERANCE/2);
         }
-        RobotActuators.climbWinch.set(_winchSpeed);
+        if (_winchTarget > getWinchPosition() + TOLERANCE)
+        {
+        RobotActuators.climbWinch.set(0.1); //TODO: Verify sign
+        }
+        if (_winchTarget < getWinchPosition() - TOLERANCE)
+        {
+            RobotActuators.climbWinch.set(-0.1);
+        }
     }
     
     public static void startSolenoid()

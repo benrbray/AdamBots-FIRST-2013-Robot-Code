b147873eb33537f4ba497093bf8a3d7653bbb305
Curtis Fenner <blueshamenblutek@gmail.com>
2013-02-13 17:29:37 -0500

    Added TTurnDegrees and TDriveDistance to the Robot Logic tasks package.

---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/tasks/TDriveDistance.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.logic.tasks;
+
+import edu.wpi.first.wpilibj.Timer;
+import robot.behavior.RobotDrive;
+import robot.logic.LogicTask;
+import robot.sensors.RobotSensors;
+import utils.MathUtils;
+
+/**
+ *
+ * @author Curtis Fenner
+ */
+public class TDriveDistance extends LogicTask {
+
+    double _targetDistanceInches;
+    double _initialEncoder;
+    boolean _iFailed = false;
+    Timer _timer = new Timer();
+    //TODO: VERIFY THAT THE ENCODERS BOTH GO FORWARD WHEN THE ROBOT GOES FORWARD
+    //OTHERWISE THIS IS USELESS.
+
+    /**
+     *
+     * @param distanceinches Determines the distance that it will go forward.
+     * Completely unintelligent by the way, it will not fail even if it gets
+     * stuck
+     */
+    TDriveDistance(double distanceinches) {
+        _targetDistanceInches = distanceinches;
+        //TODO: Insure that encoder s on wheels have accurate real world distance from get distance.
+        _timer.start();
+    }
+
+    public void initialize() {
+        _initialEncoder = (RobotSensors.encoderDriveLeft.getDistance() + RobotSensors.encoderDriveRight.getDistance()) / 2.0;
+        RobotSensors.encoderDriveLeft.start();
+        RobotSensors.encoderDriveRight.start();
+    }
+
+    public void update() {
+        RobotDrive.driveStraight(MathUtils.sign(_targetDistanceInches) * 0.1);
+        double encodertarget = _initialEncoder + _targetDistanceInches - MathUtils.sign(_targetDistanceInches);
+        double currentencoder = (RobotSensors.encoderDriveLeft.getDistance() + RobotSensors.encoderDriveRight.getDistance()) / 2.0;
+        if ((int) MathUtils.sign(encodertarget - currentencoder) != (int) MathUtils.sign(_targetDistanceInches)) {
+            RobotDrive.drive(0, 0); //Stop the robot
+            _done = true;
+        } else if (_timer.get() - 0.5 > Math.abs(_targetDistanceInches) / 5.0) {
+            _done = true;
+            _iFailed = true;
+        }
+    }
+
+    public int finish() {
+        if (_iFailed) {
+            return FAILURE;
+        } else {
+            return SUCCESS;
+        }
+    }
+}
---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/tasks/TTurnDegrees.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.logic.tasks;
+
+import robot.behavior.RobotDrive;
+import robot.logic.LogicTask;
+import robot.sensors.RobotSensors;
+
+/**
+ *
+ * @author Nathan
+ */
+public class TTurnDegrees extends LogicTask {
+
+    /**
+     * Creates a Turn Task.
+     *
+     * @param amount The amount to turn: positive or negative.
+     * @param speed The speed to turn at (wheel speed); sign ignored.
+     * @param tolerance The degree tolerance of the turn.
+     */
+    public TTurnDegrees(double amount, double speed, double tolerance) {
+        tolerance = Math.abs(tolerance * 0.5);
+        _direction = (amount > 0 ? 1.0 : -1.0);
+        _speed = Math.abs(speed) * _direction;
+        _tickCount = (amount - tolerance * _direction) * COUNT_OVER_DEGREES;
+    }
+    private double _direction;
+    private double _speed;
+    private double _tickCount = 0;
+    public static final double COUNT_OVER_DEGREES = 1.0;
+
+    public void initialize() {
+        RobotSensors.encoderDriveLeft.start();
+        RobotSensors.encoderDriveLeft.reset();
+        RobotSensors.encoderDriveRight.start();
+        RobotSensors.encoderDriveRight.reset();
+    }
+
+    public void update() {
+        double v = RobotSensors.encoderDriveLeft.get(); //TODO: Insure signs and make average
+        if (Math.abs(v) > Math.abs(_tickCount)) {
+            RobotDrive.turn(0);
+            _done = true;
+        } else {
+            RobotDrive.turn(_speed);
+        }
+    }
+
+    public int finish() {
+        return _done ? SUCCESS : FAILURE;
+    }
+}

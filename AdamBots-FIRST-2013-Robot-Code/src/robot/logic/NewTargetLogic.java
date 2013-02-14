3033b9e27d482e93aa1c364a50924e7392c50fb3
Nathan Fenner <>
2013-02-14 14:38:07 -0500

    Fixed commented out code.

---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/NewTargetLogic.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.logic;
+
+/**
+ *
+ * @author Nathan
+ */
+public class NewTargetLogic {
+	
+}

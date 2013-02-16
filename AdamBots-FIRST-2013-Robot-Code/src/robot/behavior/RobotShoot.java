ad4f7aa4b0f65035dcd2eb97687dbe7e58418b8d
Ben Bray <archon68@gmail.com>
2013-02-16 17:10:28 -0500

    Added PIDS to RobotShoot

--- AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotShoot.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotShoot.java
@@ -42,10 +42,7 @@ public abstract class RobotShoot extends RobotBehavior {
 	 * init() creates the static private _shooterPid() for controlling the shooter wheel.
 	 */
 	public static void init() {
-
-
-		_shooterPID = new FancyPIDController(
-				SHOOTER_KI, SHOOTER_KP, SHOOTER_KD,
+		_shooterPID = new FancyPIDController(SHOOTER_KI, SHOOTER_KP, SHOOTER_KD,
 				RobotSensors.counterShooterSpeed, RobotActuators.shooterWheelMotor);
 		_shooterPID.setInputRange(SHOOTER_MIN_INPUT, SHOOTER_MAX_INPUT);
 		_shooterPID.setOutputRange(SHOOTER_MIN_OUTPUT, SHOOTER_MAX_OUTPUT);

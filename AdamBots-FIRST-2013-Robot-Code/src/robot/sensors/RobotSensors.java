8d6db0bd702e14c9f37f5763338f7379c35e369b
StevenPloog <Steven@Steven-PC>
2013-02-19 16:09:43 -0500

    added config switch logic, doesnt work currently

--- AdamBots-FIRST-2013-Robot-Code/src/robot/sensors/RobotSensors.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/sensors/RobotSensors.java
@@ -236,4 +236,14 @@ public class RobotSensors extends RobotObject {
 		// TODO:  Gyro Config?
 		//gyroChassis.setSensitivity(GYRO_VPDPS);
 	}
+	
+	//// CONFIG SWITCH METHODS -------------------------------------------------
+	
+	public static boolean configOn(AnalogChannel input) {
+		if (input.getValue() > 512) {
+			return true;
+		}
+		
+		return false;
+	}
 }

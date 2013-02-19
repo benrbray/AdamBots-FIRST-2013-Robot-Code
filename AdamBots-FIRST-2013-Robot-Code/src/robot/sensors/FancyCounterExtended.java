aba76e71842a41359b2218f88b7af8353c1688a9
Tyler <Tyler@10.251.138.72>
2013-02-19 12:05:40 -0500

    Created FancyCounterExtended. Commented out getDistance() in FancyCounter class because it workis only in 1 direction

---
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/sensors/FancyCounterExtended.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.sensors;
+
+import edu.wpi.first.wpilibj.SafePWM;
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ *
+ * @author Tyler
+ */
+public class FancyCounterExtended {
+    
+    private FancyCounter fancy;
+    private SpeedController motor;
+    private int count;
+    private int currentCount;
+    private int direction;
+    private double distancePerTick;
+    
+    public FancyCounterExtended(FancyCounter f, SpeedController v) {
+	fancy = f;
+	motor = v;
+	count = 0;
+	currentCount = 0;
+	direction = 0;
+    }
+    
+    public void set(double speed) {
+	count += fancy.get() * direction;
+	motor.set(speed);
+	direction = getDirection();
+	fancy.reset();
+    }
+    
+    private int getDirection() {
+	if (motor.get() < 0) {
+	    return -1;
+	} else if (motor.get() > 0) {
+	    return 1;
+	} else {
+	    return 0;
+	}
+    }
+    
+    public int get() {
+	return count + fancy.get() * direction;
+    }
+    
+    public void setDistancePerTick(double distance) {
+	distancePerTick = distance;
+    }
+    
+    public double getDistance() {
+	return (double) get() * distancePerTick;
+    }
+}

62f3e9d1c83b2655a0e91de05b679513fbb67d19
StevenPloog <Steven@Steven-PC>
2013-02-19 12:08:02 -0500

    FancyMotor is now compatible with Jaguars. Modifying FancyMotor limit switch behavior.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/actuators/FancyMotor.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/actuators/FancyMotor.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Victor;
 
 /**
@@ -89,6 +90,26 @@ public class FancyMotor {
     public static FancyMotor createFancyVictor(int slot, int channel){
         return new FancyMotor(new Victor(slot, channel));
     }
+	
+	/**
+     * Create a new FancyMotor for a Jaguar.  
+     * @param slot The slot in the chassis that the digital module is plugged into.
+     * @param channel The PWM channel on the digital module that the Jaguar is attached to.
+     * @return A new FancyMotor object.
+     */
+	public static FancyMotor createFancyTalon(int slot, int channel){
+		return new FancyMotor(new Talon(slot, channel));
+	}
+	
+	/**
+     * Create a new FancyMotor for a Jaguar.  
+     * @param slot The slot in the chassis that the digital module is plugged into.
+     * @param channel The PWM channel on the digital module that the Jaguar is attached to.
+     * @return A new FancyMotor object.
+     */
+	public static FancyMotor createFancyTalon(int channel) {
+		return new FancyMotor(new Talon(channel));
+	}
     
     //// INITIALIZATION --------------------------------------------------------
     
@@ -106,4 +127,26 @@ public class FancyMotor {
     }
     
     //// GETTER / SETTER METHODS -----------------------------------------------
+	
+	/**
+	 * Set the limit switch used to stop the motor from moving in the POSITIVE
+	 * direction when the switch is pressed.
+	 * @param upperLimit A reference to a limit switch (DigitalInput) instance.
+	 * @see edu.wpi.first.wpilibj.DigitalInput
+	 * @see robot.sensors.RobotSensors
+	 */
+	public void setUpperLimit(DigitalInput upperLimit){
+		_upperLimit = upperLimit;
+	}
+	
+	/**
+	 * Set the limit switch used to stop the motor from moving in the NEGATIVE
+	 * direction when the switch is pressed.
+	 * @param lowerLimit A reference to a limit switch (DigitalInput) instance.
+	 * @see edu.wpi.first.wpilibj.DigitalInput
+	 * @see robot.sensors.RobotSensors
+	 */
+	public void setLowerLimit(DigitalInput lowerLimit){
+		_lowerLimit = lowerLimit;
+	}
 }

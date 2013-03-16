dd5bc045664b39ac9e6f2ea3dc6813080d5ec89c
Nathan Fenner <>
2013-03-16 18:23:40 -0400

    Added documentation and cleaned up a little bit in RobotShoot.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotShoot.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/behavior/RobotShoot.java
@@ -9,9 +9,7 @@ import robot.actuators.RobotActuators;
 import robot.camera.RobotCamera;
 import robot.control.MagicBox;
 import robot.logic.FancyPIDController;
-import robot.logic.targeting.TargetShooterAngleLogic;
 import robot.sensors.RobotSensors;
-import utils.MathUtils;
 
 /**
  * RobotShoot sets the speed and angle of the shooter; update() must be called
@@ -41,6 +39,10 @@ public abstract class RobotShoot extends RobotBehavior {
 	 * The PID interface used to control the shooter.
 	 */
 	private static FancyPIDController _shooterPID;
+	/**
+	 * Turns on and off the ability to set target angle.
+	 */
+	private static boolean _moveToTarget = false;
 
 	/**
 	 * init() creates the static private _shooterPid() for controlling the shooter wheel.
@@ -53,44 +55,45 @@ public abstract class RobotShoot extends RobotBehavior {
 		_shooterPID.setPercentTolerance(SHOOTER_PID_TOLERANCE);
 		_shooterPID.enable();
 	}
-	
+
 	/**
 	 * Calculates and returns the shooter angle from the string potentiometer.
 	 * @return angle of elevation (above horizontal).
 	 */
-	public static double getShooterAngleDegrees()
-	{
-		double x = (RobotSensors.stringPot.getAverageVoltage()-4.88)/-0.8156; // Length of string in inches
-		double y = 12.0 + 3/8.0; // Length of triangle side #1
-		double z = 12.0 + 1/2.0; // Length of triangle side #2
-		return com.sun.squawk.util.MathUtils.acos(Math.min(1,Math.max(0,(x*x-y*y-z*z)/(-2.0*y*z))))*180.0/Math.PI + 19;//TODO: Use potentiometer calculations
+	public static double getShooterAngleDegrees() {
+		double x = (RobotSensors.stringPot.getAverageVoltage() - 4.88) / -0.8156; // Length of string in inches
+		double y = 12.0 + 3 / 8.0; // Length of triangle side #1
+		double z = 12.0 + 1 / 2.0; // Length of triangle side #2
+		return com.sun.squawk.util.MathUtils.acos(Math.min(1, Math.max(0, (x * x - y * y - z * z) / (-2.0 * y * z)))) * 180.0 / Math.PI + 19;//TODO: Use potentiometer calculations
 	}
-	
-	public static double getIdealShooterAngle()
-	{
+
+	/**
+	 * Calculates the ideal angle, in degrees as calculated from the RobotCamera's distance value.
+	 * @return Degree automatically calculated angle for best shooting.
+	 */
+	public static double getIdealShooterAngle() {
 		double x = RobotCamera.getTargetLocationUnits();
-		double a = .0002195378;
+		double a = .0002195378;//Constants determined from quadratic best fit.
 		double b = -0.1315442966;
 		double c = 37.98049416;
-		return a*x*x + b*x + c;
+		return a * x * x + b * x + c;
 	}
-	
+
 	/**
-	 * Sets the target angle of elevation to go for while TargetShooterAngleLogic.isTargeting() is true.
+	 * Sets the target angle of elevation to go for after  is true.
 	 * @param angle The target angle of elevation.
 	 */
-	public static void setTargetAngleDegrees(double angle)
-	{
+	public static void setTargetAngleDegrees( double angle ) {
 		_targetAngleDegrees = angle;
 	}
-	
+
 	/**
-	 * Called periodically to control the shooterAngle motor.
+	 * Tells whether the shooter's angle is close to the target angle.
 	 */
 	public static boolean isShooterInPosition() {
 		return Math.abs(getShooterAngleDegrees() - _targetAngleDegrees) < SHOOTER_ANGLE_TOLERANCE;
 	}
- 
+
 	/**
 	 * Returns whether or not the PID on the shooter motor reports it's at speed.
 	 * @return Whether or not the PID on the shooter motor reports it's at speed.
@@ -106,66 +109,60 @@ public abstract class RobotShoot extends RobotBehavior {
 	public static void setSpeed( double speed_rpm ) {
 		_shooterPID.setRPM(speed_rpm);
 	}
-	
+
+	/**
+	 * Disables pid.
+	 */
 	public static void stopPID() {
-		if (_shooterPID.isEnable()) {
+		if ( _shooterPID.isEnable() ) {
 			_shooterPID.disable();
 		}
 	}
-	
+
+	/**
+	 * Starts pid.
+	 */
 	public static void startPID() {
-		if (!_shooterPID.isEnable()) {
+		if ( !_shooterPID.isEnable() ) {
 			_shooterPID.enable();
 		}
 	}
-	
+
+	/**
+	 * Provides new pid constants for the pid.
+	 */
 	public static void updatePIDConstants() {
 		_shooterPID.setPID(SHOOTER_KP, SHOOTER_KI, SHOOTER_KD);
 	}
-	
-	public static void setTargetAngle(double ang)
-	{
-		_targetAngleDegrees = ang;
-	}
-	
-	private static boolean _moveToTarget = false;
-	
-	public static void startMovingToTarget()
-	{
-		_moveToTarget = true;
-	}
-	public static void stopMovingToTarget()
-	{
-		_moveToTarget = false;
-	}
-	
-	
 
 	/**
-	 * To be called constantly. Adjusts the angle of the shooter if TargetShooterAngleLogic.isTargeting().
-	 * Tries to move shooter to match the value set by setShooterAngleDegrees(double).
+	 * Enables control over the target angle.
+	 */
+	public static void startMovingToTarget() {
+		_moveToTarget = true;
+	}
+
+	/**
+	 * Disables control over the target angle.
+	 */
+	public static void stopMovingToTarget() {
+		_moveToTarget = false;
+	}
+
+	/**
+	 * Adjusts shooter angle if .startMovingToTarget() has been called or Magic Box Siwtch #7 is on.
 	 */
 	public static void update() {
-		if (RobotMain.getInstance().isAutonomous())
-		{
-			if ( Math.abs(getShooterAngleDegrees() - _targetAngleDegrees) < SHOOTER_ANGLE_TOLERANCE ) {
+		if ( _moveToTarget || (!RobotMain.getInstance().isAutonomous() && MagicBox.getDigitalIn(7)) ) {
+			if ( !RobotMain.getInstance().isAutonomous() && MagicBox.getDigitalIn(7) ) {
+				_targetAngleDegrees = getIdealShooterAngle();
+			}
+			double target = Math.max(19, _targetAngleDegrees);
+			if ( Math.abs(getShooterAngleDegrees() - target) < SHOOTER_ANGLE_TOLERANCE ) {
 				RobotActuators.shooterAngleMotor.set(0);
 			}
 			else {
-				RobotActuators.shooterAngleMotor.set(-MathUtils.sign((_targetAngleDegrees - getShooterAngleDegrees()) / 40.0));
-			}
-		}
-		else
-		{
-			if (MagicBox.getDigitalIn(7))
-			{
-				_targetAngleDegrees = getIdealShooterAngle();
-				if ( Math.abs(getShooterAngleDegrees() - _targetAngleDegrees) < SHOOTER_ANGLE_TOLERANCE ) {
-					RobotActuators.shooterAngleMotor.set(0);
-				}
-				else {
-					RobotActuators.shooterAngleMotor.set(-MathUtils.sign((_targetAngleDegrees - getShooterAngleDegrees()) / 40.0));
-				}
+				RobotActuators.shooterAngleMotor.set(-utils.MathUtils.sign(target - getShooterAngleDegrees()) / 40.0);
 			}
 		}
 	}

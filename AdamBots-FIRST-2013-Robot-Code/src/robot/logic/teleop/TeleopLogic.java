578d7f63b862ce90d49589c3c6a76136cec7e579
StevenPloog <Steven@Steven-PC>
2013-03-16 14:36:15 -0400

    Removed second speed controller for climbing winch.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/logic/teleop/TeleopLogic.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/teleop/TeleopLogic.java
@@ -187,24 +187,24 @@ public class TeleopLogic extends LogicPhase {
 			if (_setWinch < 0) {
 				RobotActuators.climbWinch.set(_setWinch);
 				
-				RobotActuators.climbWinch2.set(RobotActuators.climbWinch.get());
+				//RobotActuators.climbWinch2.set(RobotActuators.climbWinch.get());
 				
 				RobotActuators.climbWinchSolenoid.set(Relay.Value.kOff);
 			} else if (_primaryButtons[FancyJoystick.BUTTON_BACK]) {
 				RobotActuators.climbWinchSolenoid.set(Relay.Value.kForward);
 				RobotActuators.climbWinch.set(_setWinch);
 				
-				RobotActuators.climbWinch2.set(RobotActuators.climbWinch.get());
+				//RobotActuators.climbWinch2.set(RobotActuators.climbWinch.get());
 			} else {
 				RobotActuators.climbWinchSolenoid.set(Relay.Value.kOff);
 				RobotActuators.climbWinch.set(0);
-				RobotActuators.climbWinch2.set(0);
+				//RobotActuators.climbWinch2.set(0);
 			}
 		} else {
 			//TODO: Set winch drive to 0
 			RobotActuators.climbWinchSolenoid.set(Relay.Value.kOff);
 			RobotActuators.climbWinch.set(0);
-			RobotActuators.climbWinch2.set(0);
+			//RobotActuators.climbWinch2.set(0);
 		}
 
 		SmartDashboard.putNumber("winchVoltage", RobotActuators.climbWinch.get());

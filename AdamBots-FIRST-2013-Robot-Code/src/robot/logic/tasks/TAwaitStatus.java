475e1079526d69a5a72fa6cbf3988ab2adf9ff2f
Ben Bray <archon68@gmail.com>
2013-03-18 22:22:28 -0400

    Added timeout option to TAwaitStatus.

--- AdamBots-FIRST-2013-Robot-Code/src/robot/logic/tasks/TAwaitStatus.java
+++ AdamBots-FIRST-2013-Robot-Code/src/robot/logic/tasks/TAwaitStatus.java
@@ -19,14 +19,21 @@ import robot.sensors.RobotSensors;
 public final class TAwaitStatus extends LogicTask {
     //// CONSTANTS -------------------------------------------------------------
     
+	// Status Constants
     public static final int WINCH_IN_POSITION = 0;
     public static final int SHOOTER_IN_POSITION = 1;
     public static final int SHOOTER_UP_TO_SPEED = 2;
     public static final int TARGETING_COMPLETED = 98;
+	
+	// Timeout
+	public static final int NO_TIMEOUT = -1;
     
     //// PRIVATE VARIABLES -----------------------------------------------------
     
     private int _status;
+	private long _timeout;
+	
+	private long _initialTimeMillis;
     
     //// CONSTRUCTOR -----------------------------------------------------------
     
@@ -37,18 +44,40 @@ public final class TAwaitStatus extends LogicTask {
      * @param status The status to wait for.
      */
     public TAwaitStatus(int status){
+		this(status, NO_TIMEOUT);
+    }
+	
+	/**
+	 * This task will wait for the specified status to be achieved.  For example, it
+     * may be configured to wait until the shooter is up to speed before telling the
+     * current LogicPhase to move on to the next task.  
+	 * @param status The status to wait for.
+	 * @param timeoutMillis If the status is not reached after the elapsed time
+	 * has exceeded this amount, then the task reports that it has been successfully
+	 * completed.  To remove the time restriction, set this value to TAwaitStatus.NO_TIMEOUT.
+	 * @see TAwaitStatus#NO_TIMEOUT
+	 */
+	public TAwaitStatus(int status, int timeoutMillis){
 		_status = status;
+		_timeout =  timeoutMillis;
     }
 
     //// INITIALIZATION --------------------------------------------------------
     
+	/**
+	 * Initializes this task.  Stores the current system time, for timeout purposes.
+	 */
     public void initialize() {
-	
+		_initialTimeMillis = System.currentTimeMillis();
     }
 
     //// UPDATE ----------------------------------------------------------------
     
+	/**
+	 * Checks to see if the status has been achieved or if the task has timed out.
+	 */
     public void update() {
+		// Check Status
 		switch(_status){
 			case WINCH_IN_POSITION:
 				_done = RobotClimb.isWinchInPosition();
@@ -59,6 +88,11 @@ public final class TAwaitStatus extends LogicTask {
 			case SHOOTER_UP_TO_SPEED:
 				_done = RobotShoot.isShooterUpToSpeed();
 				break;
+		}
+		
+		// Timeout
+		if(_timeout > 0 && System.currentTimeMillis() - _initialTimeMillis >= _timeout){
+			_done = true;
 		}
     }
 
